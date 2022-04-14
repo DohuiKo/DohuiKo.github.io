98ee1451d3f147f2233dd45fc6d5d6d327a6dc80
Dohui Ko <bluecard722@hufs.ac.kr>
2022-04-14 13:05:28 +0900

    Add: 깃허브 버전이란?

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,7 +67,7 @@ module.exports = {
       {
         title: 'Github',
         collapsable: true,
-        children: ['/Github/Github 처음 생성하고 연결하기']
+        children: ['/Github/Github 처음 생성하고 연결하기', '/Github/원격 저장소 만들어서 로컬 저장소에 연결하기']
       },
     ]
   },
